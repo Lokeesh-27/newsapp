abd71933e86a027dc02e46ea2aecbeb3ee59e8ff
Lokeesh-27 <74608585+Lokeesh-27@users.noreply.github.com>
2020-11-20 21:20:18 +0530

    Update settings.py

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'i^fs+x7oi#$u(s%(24@!$ub&jlr++c+1qnd%+o1zq$dq(r2u#u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['news1app.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
